4879e2c150623ec1b0266cfe6432034dcbf47a04
Vincent van den Boogaard <vincent@wemakeitspark.nl>
2021-12-15 16:27:33 +0100

    Update config

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,14 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf5a1d1fb552e5256401ce94ee6cbab9b
 {
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->classMap = ComposerStaticInitf5a1d1fb552e5256401ce94ee6cbab9b::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7ecfa2c58365724cdddccc762c0d55fc7d57537b',
+    'name' => 'freshwave/onaccountpayment',
+  ),
+  'versions' => 
+  array (
+    'freshwave/onaccountpayment' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7ecfa2c58365724cdddccc762c0d55fc7d57537b',
+    ),
+  ),
+);
